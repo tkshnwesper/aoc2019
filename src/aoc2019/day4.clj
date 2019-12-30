bbe828fff3fb8942744f8c8956b30a53fc233fe9
Saurabh Machave <unknown7bolt@gmail.com>
2019-12-30 12:29:11 +0530

    complete day4 part2

--- src/aoc2019/day4.clj
+++ src/aoc2019/day4.clj
@@ -2,7 +2,7 @@
 
 (def inputs '(172930 683082))
 
-(defn meets-criteria?
+(defn meets-criteria-1?
   [lower-bound upper-bound number]
   (let [number-string (.toString number)]
     (and
@@ -19,8 +19,35 @@
                               "$"))
                  number-string))))))
 
+(defn meets-criteria-2?
+  [lower-bound upper-bound number]
+  (let [number-string (.toString number)]
+    (and
+     (= (count number-string) 6)
+     (and
+      (>= number lower-bound)
+      (<= number upper-bound))
+     (not (empty? (filter
+                   (partial = 2)
+                   (map
+                    (comp count first)
+                    (re-seq #"(\d)\1+" number-string)))))
+     (not (nil? (re-find
+                 (re-pattern (str
+                              "^"
+                              (apply str (for [i (range 1 10)]
+                                           (str (.toString i) "*")))
+                              "$"))
+                 number-string))))))
+
 (defn day4-part1-solution 
   []
   (count
    (filter true? (for [i (apply range inputs)]
-     ((apply partial meets-criteria? inputs) i)))))
+     ((apply partial meets-criteria-1? inputs) i)))))
+
+(defn day4-part2-solution 
+  []
+  (count
+   (filter true? (for [i (apply range inputs)]
+                   ((apply partial meets-criteria-2? inputs) i)))))
--- test/aoc2019/day4_test.clj
+++ test/aoc2019/day4_test.clj
@@ -4,20 +4,24 @@
 
 (deftest test-number-is-within-bounds
   (testing "122222 should return false for bounds 500000-1000000"
-    (is (false? (aoc2019.day4/meets-criteria? 500000 1000000 122222))))
+    (is (false? (aoc2019.day4/meets-criteria-1? 500000 1000000 122222))))
   (testing "112222 should return true for bounds 500-2000000"
-    (is (true? (aoc2019.day4/meets-criteria? 500 2000000 112222)))))
+    (is (true? (aoc2019.day4/meets-criteria-1? 500 2000000 112222)))))
 
 (deftest test-two-adjacent-digits-are-the-same
   (testing "10 should fail"
-    (is (false? (aoc2019.day4/meets-criteria? 0 20 10))))
+    (is (false? (aoc2019.day4/meets-criteria-1? 0 20 10))))
   (testing "12 should fail"
-    (is (false? (aoc2019.day4/meets-criteria? 0 20 12)))))
+    (is (false? (aoc2019.day4/meets-criteria-1? 0 20 12)))))
 
 (deftest test-digits-are-increasing
   (testing "221 should fail"
-    (is (false? (aoc2019.day4/meets-criteria? 100 300 221)))))
+    (is (false? (aoc2019.day4/meets-criteria-1? 100 300 221)))))
 
 (deftest test-number-of-digits-should-be-6
   (testing "12344 should fail"
-    (is (false? (aoc2019.day4/meets-criteria? 0 66666 12344)))))
+    (is (false? (aoc2019.day4/meets-criteria-1? 0 66666 12344)))))
+
+(deftest test-two-adjacent-digits-are-not-part-of-larger-group
+  (testing "123444 does not meet criteria"
+    (is (false? (aoc2019.day4/meets-criteria-2? 0 666666 123444)))))
